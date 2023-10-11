eb447ea2f611d05babe2565892df7955270ab498
Olga Korol <oggi.fine@gmail.com>
2023-10-11 20:38:58 +0300

    test1 changed

--- src/test/java/CreditCardTest.java
+++ src/test/java/CreditCardTest.java
@@ -1,25 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditCardTest {
     private WebDriver driver;
-    private  WebDriverManager;
+    private  WebDriverManager driverManager;
 
     @BeforeAll
     public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
+      WebDriverManager.chromedriver().setup();
     }
     @BeforeEach
     public void beforeEach() {
-        СhromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+//         driver.get("http://localhost:9999");
 
     }
 
@@ -30,9 +37,24 @@ public class CreditCardTest {
 
     }
     @Test
-    void shouldTest() {
+    void shouldTest() throws InterruptedException {
+        // загрузить страницу
         driver.get("http://localhost:9999");
-        Thread.sleep(5000);
+        // поиск элементов
+      driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петрова Мария");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79782226677");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+//     var actualText1 = driver.findElement(By.className("order-success")).getText().trim();
+     assertEquals(  "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+//        assertEquals(  "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText1)
+
+
+
+
+
+
     }
 
 
