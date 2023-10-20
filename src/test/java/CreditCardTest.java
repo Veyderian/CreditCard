284ed2555b2178ce626bbaa3b8c593fdf35db2ac
Olga Korol <oggi.fine@gmail.com>
2023-10-20 10:50:16 +0300

    reformat code

--- src/test/java/CreditCardTest.java
+++ src/test/java/CreditCardTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -46,37 +45,9 @@ public class CreditCardTest {
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button.button")).click();
         var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-//     var actualText1 = driver.findElement(By.className("order-success")).getText().trim();
      assertEquals(  "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-//        assertEquals(  "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText1)
+
 
     }
 
-//    @Test //2
-//    void shouldNegativeTest() throws InterruptedException {
-//
-//        driver.get("http://localhost:9999");
-//
-//        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79782226677");
-//        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-//        driver.findElement(By.cssSelector("button.button")).click();
-////       var actualText = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid. input__sub")).getText().trim();
-//        var actualText = driver.findElement(By.xpath("//span[@data-test-id='name'] [label (@class, 'input_invalid')//span [@class, 'input__sub']")).getText().trim();
-//        assertEquals("Поле обязательно для заполнения", actualText);
-//
-//    }
-
-//    @Test //3
-//    void shouldNegativeTest1() throws InterruptedException {
-//        // загрузить страницу
-//        driver.get("http://localhost:9999");
-//        // поиск элементов
-//        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петрова Мария");
-//        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79782226677");
-//        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-//        driver.findElement(By.cssSelector("button.button")).click();
-//        var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-//
-//        assertEquals(  "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText;
-    }
-//}
+}
