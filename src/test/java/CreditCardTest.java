dda4674ae58184b1957abab8d353ae904d3bef7d
Olga Korol <oggi.fine@gmail.com>
2023-10-23 13:07:04 +0300

    reformat code

--- src/test/java/CreditCardTest.java
+++ src/test/java/CreditCardTest.java
@@ -25,11 +25,9 @@ public class CreditCardTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-//         driver.get("http://localhost:9999");
-
     }
 
-@AfterEach
+    @AfterEach
     public void AfterEach() {
         driver.quit();
         driver = null;
@@ -40,14 +38,12 @@ public class CreditCardTest {
         // загрузить страницу
         driver.get("http://localhost:9999");
         // поиск элементов
-      driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петрова Мария");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петрова Мария");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79782226677");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button.button")).click();
         var actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-     assertEquals(  "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
 
     }
-
 }
